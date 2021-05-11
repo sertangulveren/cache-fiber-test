97b7b2d74a77665aef262b77eb5552c2a4530d7f
Sertan Gülveren <sertangulveren@gmail.com>
2021-05-11 23:55:22 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cache-fiber-test
+
+go 1.16
+
+require github.com/gofiber/fiber/v2 v2.9.0 // indirect
---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"log"
+	"net/http"
+	"time"
+)
+
+type Campaign struct {
+	Code string `json:"code"`
+	Name string `json:"name"`
+}
+
+var campaigns = map[string][]Campaign{
+	"west-europe": []Campaign{
+		{"WE-C1", "Buy a bike, get 1 liter of gasoline for free!"},
+		{"WE-C2", "Buy a car, get driving school training for free!"},
+		{"WE-C3", "Buy a jackboot, get 3 bikinis for free!"},
+		{"WE-C4", "All products are 1% off!"},
+	},
+	"east-europe": []Campaign{
+		{"EE-C1", "All products are 5% off!"},
+	},
+}
+
+var queryCount int
+
+func main() {
+	app := fiber.New()
+	app.Use(cache.New(cache.Config{
+		KeyGenerator: func(c *fiber.Ctx) string {
+			return getCacheKey(c)
+		},
+		Expiration: time.Second * 30,
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("refresh") == "true"
+		},
+	}))
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		results := FetchCampaignsFromDB(getCacheKey(c))
+		return c.Status(fiber.StatusOK).JSON(results)
+	})
+
+	app.Listen(":3000")
+}
+
+func getCacheKey(c *fiber.Ctx) string {
+	key := c.Query("region", "west-europe")
+	return key
+}
+
+func FetchCampaignsFromDB(key string) []Campaign {
+	queryCount++
+	fmt.Println("Sorgulama Başlatıldı")
+	fmt.Printf("Sorgulama Sayısı: %d\n", queryCount)
+	results := campaigns[key]
+	// 3 saniyelik bir fetch işlemi yapıyoruz. Amaç latency olsun.
+	_, err := http.Get("https://deelay.me/3000/https://www.google.com")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return results
+}
